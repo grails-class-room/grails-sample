735dc08a307d6078481ee17896b4d2296155b44c
Romulo Fagundes Cantanhede <romulo.fagundes@webonesystem.com.br>
2016-10-27 07:56:35 -0300

    Adicionando o Bower com as respectivas dependências. Executado: grails> gradle bowerInstall Referenciando os arquivos CSS e JS instalados pelo Bower.

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -5,17 +5,22 @@
 // You're free to add application-wide JavaScript to this file, but it's generally better
 // to create separate JavaScript files as needed.
 //
-//= require jquery-2.2.0.min
-//= require bootstrap
-//= require_tree .
+//= require jquery/jquery.js
+//= require bootstrap/js/bootstrap.js
+//= require vue/vue.js
+//= require vue-form/vue-form.js
+//= require vue-resource/vue-resource.js
+//= require bootstrap-datepicker/bootstrap-datepicker.min.js
+//= require bootstrap-datepicker/bootstrap-datepicker.pt-BR.min.js
+//= require vue-validator/vue-validator.js
 //= require_self
 
 if (typeof jQuery !== 'undefined') {
     (function($) {
-        $(document).ajaxStart(function() {
-            $('#spinner').fadeIn();
+        $('#spinner').ajaxStart(function() {
+            $(this).fadeIn();
         }).ajaxStop(function() {
-            $('#spinner').fadeOut();
+            $(this).fadeOut();
         });
     })(jQuery);
 }
